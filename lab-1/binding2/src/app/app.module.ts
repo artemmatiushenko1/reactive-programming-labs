1830d650e7c4a7eb7e0eccb2458f16a01356f05a
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-18 22:12:15 +0300

    feat: + task 7

--- lab-1/binding2/src/app/app.module.ts
+++ lab-1/binding2/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { TaskWrapperComponent } from './task-wrapper/task-wrapper.component';
 import { Task6Component } from './task6/task6.component';
 import { FormsModule } from '@angular/forms';
+import { Task7Component } from './task7/task7.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskWrapperComponent, Task6Component],
+  declarations: [AppComponent, TaskWrapperComponent, Task6Component, Task7Component],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ lab-1/binding2/src/app/task7/task7.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task7',
+  templateUrl: './task7.component.html',
+  styleUrls: ['./task7.component.css'],
+})
+export class Task7Component {
+  isyellow = false;
+}
