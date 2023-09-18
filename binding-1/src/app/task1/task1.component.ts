c7c68d11fd097265e446fae4564b47a7abe4cae9
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-18 21:16:01 +0300

    feat: + binding-1 app

---
+++ binding-1/src/app/task1/task1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task1',
+  templateUrl: './task1.component.html',
+})
+export class Task1Component {
+  name = 'Tom';
+  age = 25;
+}
