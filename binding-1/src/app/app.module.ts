a74e50ff61c208371d1c376d0f910c04bf9b4997
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-09-18 21:26:04 +0300

    feat: + task 3

--- binding-1/src/app/app.module.ts
+++ binding-1/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Task1Component } from './task1/task1.component';
 import { Task2Component } from './task2/task2.component';
+import { Task3Component } from './task3/task3.component';
 
 @NgModule({
-  declarations: [AppComponent, Task1Component, Task2Component],
+  declarations: [AppComponent, Task1Component, Task2Component, Task3Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ binding-1/src/app/task3/task3.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task3',
+  templateUrl: './task3.component.html',
+})
+export class Task3Component {
+  colspan = 2;
+}
