3d922ce494d5eed24bba76f0312636637050460f
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-10-03 22:07:36 +0300

    feat: + task 10

--- lab-2/components4/src/app/app.component.ts
+++ lab-2/components4/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'components4';
+export class AppComponent implements OnInit, OnDestroy {
+  name = 'Tom';
+  age = 25;
+
+  constructor() {
+    this.log('constructor');
+  }
+
+  ngOnInit() {
+    this.log('onInit');
+  }
+
+  ngOnDestroy() {
+    this.log('onDestroy');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (let propName in changes) {
+      let chng = changes[propName];
+      let cur = JSON.stringify(chng.currentValue);
+      let prev = JSON.stringify(chng.previousValue);
+      this.log(
+        `Parent [${propName}]: currentValue = ${cur}, previousValue = ${prev}`
+      );
+    }
+  }
+
+  private log(msg: string) {
+    console.log(msg);
+  }
 }
--- lab-2/components4/src/app/app.module.ts
+++ lab-2/components4/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ChildComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
