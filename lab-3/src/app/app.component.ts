19efacf5bbceb8cb39b948694b93551fa41d6531
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-10-11 21:52:10 +0300

    feat: + create todo form

--- lab-3/src/app/app.component.ts
+++ lab-3/src/app/app.component.ts
@@ -24,4 +24,10 @@ export class AppComponent {
   get items(): readonly TodoItem[] {
     return this.list.items.filter((item) => !item.complete);
   }
+
+  addItem(newItem: string) {
+    if (newItem != '') {
+      this.list.addItem(newItem);
+    }
+  }
 }
