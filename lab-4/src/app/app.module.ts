268d5110438e93d521927387d5f263ee4bb8e01d
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-10-28 18:07:06 +0300

    feat: + post form markup

--- lab-4/src/app/app.module.ts
+++ lab-4/src/app/app.module.ts
@@ -2,23 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
+
+// NZ imports
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostFormComponent, PostComponent],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    // HttpClientModule,
+    NzInputModule,
+    NzButtonModule,
+    NzIconModule,
+    NzSpaceModule,
+    HttpClientModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
