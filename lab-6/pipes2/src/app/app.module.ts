17e6b5863ce80ee028f90e53a290c13a6b9628cb
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-10-31 22:03:05 +0200

    feat: + task 2

--- lab-6/pipes2/src/app/app.module.ts
+++ lab-6/pipes2/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormatPipe } from './format.pipe';
+import { FormsModule } from '@angular/forms';
+import { JoinPipe } from './join.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FormatPipe, JoinPipe],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
