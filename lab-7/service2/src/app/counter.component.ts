8e0efeed91c57c37fd8aacebcdb29eb72e14638c
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-11-05 14:13:41 +0200

    feat: + task 5

--- lab-7/service2/src/app/counter.component.ts
+++ lab-7/service2/src/app/counter.component.ts
@@ -3,26 +3,22 @@ import { AppCounterService } from './services/app-counter.service';
 import { LocalCounterService } from './services/local-counter.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-counter',
   template: `
-    <h1>Компонент app.component.ts</h1>
+    <h1>Компонент counter.component.ts</h1>
     <h2>Сервіс верхнього рівня App Counter {{ appCounterService.counter }}</h2>
     <button class="btn" (click)="appCounterService.increase()">+</button>
     <button class="btn" (click)="appCounterService.decrease()">-</button>
-    <hr />
     <h2>
       Сервіс рівня компоненту Local Counter {{ localCounterService.counter }}
     </h2>
     <button class="btn" (click)="localCounterService.increase()">+</button>
     <button class="btn" (click)="localCounterService.decrease()">-</button>
     <hr />
-    <app-counter></app-counter>
   `,
   providers: [LocalCounterService],
 })
-export class AppComponent {
-  title = 'service2';
-
+export class CounterComponent {
   constructor(
     protected appCounterService: AppCounterService,
     protected localCounterService: LocalCounterService
