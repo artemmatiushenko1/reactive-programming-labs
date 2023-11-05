c127a172cd232195ae08cdb28f41355de8ce59fa
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-11-05 13:56:56 +0200

    feat: + task 4

--- lab-7/service1/src/app/app.component.ts
+++ lab-7/service1/src/app/app.component.ts
@@ -1,79 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DataService } from './data.service';
-import { Phone } from './phone.model';
 import { LogService } from './log.service';
 
 @Component({
   selector: 'app-root',
   providers: [DataService, LogService],
   template: `
-    <div class="row">
-      <input
-        class="form-control cardinput"
-        [(ngModel)]="name"
-        placeholder="Модель"
-      />
-      <input
-        type="number"
-        class="form-control cardinput"
-        [(ngModel)]="price"
-        placeholder="Ціна"
-      />
-      <button class="btn btn-default cardinput" (click)="addItem(name, price)">
-        Додати
-      </button>
-    </div>
-    <table>
-      <thead>
-        <tr>
-          <th class="cardinput">Модель</th>
-          <th class="cardinput">Ціна</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr *ngFor="let item of items">
-          <td class="cardinput">{{ item.name }}</td>
-          <td class="cardinput">{{ item.price }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <data-comp></data-comp>
+    <data-comp></data-comp>
   `,
-  styles: [
-    `
-      .cardinput {
-        margin: 0.4rem 1rem;
-      }
-      td {
-        padding: 5px;
-        margin: 0.4rem 1.8rem;
-      }
-      table {
-        width: 100%;
-        overflow: auto;
-        color: rgb(0, 0, 0);
-        border-spacing: 1px;
-        table-layout: fixed;
-      }
-      th {
-        padding: 5px;
-        margin: 0.4rem 1.8rem;
-        text-align: left;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  'price': number;
-  name: string = '';
-  items: Phone[] = [];
-
-  constructor(private dataService: DataService) {}
-
-  addItem(name: string, price: number) {
-    this.dataService.addData(name, price);
-  }
-
-  ngOnInit() {
-    this.items = this.dataService.getData();
-  }
-}
+export class AppComponent {}
--- lab-7/service1/src/app/app.module.ts
+++ lab-7/service1/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { DataComponent } from './data.component';
+import { DataService } from './data.service';
+import { LogService } from './log.service';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DataComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
