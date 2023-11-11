73ad3de7e3ff9ca5a67800b0399d111db2fe3f76
artemmatiushenko1 <artom.matyushenko@gmail.com>
2023-11-11 17:32:15 +0200

    feat: + checkout flow

--- lab-8/src/app/model/static.datasource.ts
+++ lab-8/src/app/model/static.datasource.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product.model';
 import { from, Observable } from 'rxjs';
+import { Order } from './order.model';
 
 @Injectable()
 class StaticDataSource {
@@ -24,6 +25,11 @@ class StaticDataSource {
 
   getProducts(): Observable<Product[]> {
     return from([this.products]);
+  }
+
+  saveOrder(order: Order): Observable<Order> {
+    console.log(JSON.stringify(order));
+    return from([order]);
   }
 }
 
